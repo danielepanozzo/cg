2628aad456da152d120e65a0a1aba4e599d3e049
Nare Jayanth Kumar Reddy <reddyj@jayanth-macbook.local>
2023-02-04 00:09:47 -0500

    first semi working sol

--- Assignment_1/src/hull/main.cpp
+++ Assignment_1/src/hull/main.cpp
@@ -11,33 +11,52 @@ typedef std::complex<double> Point;
 typedef std::vector<Point> Polygon;
 
 double inline det(const Point &u, const Point &v) {
-	// TODO
-	return 0;
+	return u.real()*v.imag() - u.imag()*v.real();
 }
 
 struct Compare {
 	Point p0; // Leftmost point of the poly
 	bool operator ()(const Point &p1, const Point &p2) {
-		// TODO
-		return true;
+        double cross_product = det(p1-p0, p2-p0);
+        if(cross_product==0) {
+            return norm(p2-p0) >= norm(p1-p0);
+        }
+		return cross_product>0;
 	}
 };
 
 bool inline salientAngle(Point &a, Point &b, Point &c) {
-	// TODO
-	return false;
+	return det(b-a, c-b)>=0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 Polygon convex_hull(std::vector<Point> &points) {
 	Compare order;
-	// TODO
-	order.p0 = Point(0, 0);
+
+    // Scanning all points to find the point p0 with lowest y coordinate
+    double p0_y = points.at(0).imag();
+    double p0_x = points.at(0).real();
+    for (int i=0; i<points.size(); i++) {
+        if(points.at(i).imag() < p0_y) {
+            p0_y = points.at(i).imag();
+            p0_x = points.at(i).real();
+        } else if (points.at(i).imag() == p0_y && points.at(i).real() < p0_x) {
+            p0_y = points.at(i).imag();
+            p0_x = points.at(i).real();
+        }
+    }
+    order.p0 = Point(p0_x, p0_y);
+
 	std::sort(points.begin(), points.end(), order);
 	Polygon hull;
-	// TODO
-	// use salientAngle(a, b, c) here
+	for(int i=0; i<points.size(); i++) {
+        while(hull.size()>2 && !salientAngle(hull.at(hull.size()-2), hull.back(), points.at(i))) {
+            hull.pop_back();
+        }
+        hull.push_back(points.at(i));
+    }
+
 	return hull;
 }
 
@@ -46,7 +65,14 @@ Polygon convex_hull(std::vector<Point> &points) {
 std::vector<Point> load_xyz(const std::string &filename) {
 	std::vector<Point> points;
 	std::ifstream in(filename);
-	// TODO
+	std::string line;
+	std::getline(in, line);
+	int n_points = std::stoi(line);
+    float x, y, z;
+    for (int i=0; i<n_points; i++) {
+        in >> x >> y >> z;
+        points.push_back(Point(x, y));
+    }
 	return points;
 }
 
